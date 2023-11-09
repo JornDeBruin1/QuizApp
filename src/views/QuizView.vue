8107cb30697b73e40a44ed9a5dc855ede6eeb299
Jorn de Bruin <jorn.bruin@student.graafschapcollege.nl>
2023-11-09 10:27:26 +0100

    question working and a result page

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,99 +1,57 @@
+<script setup>
+import Question from '../components/Question.vue'
+import QuizHeader from '../components/QuizHeader.vue'
+import Result from '../components/Result.vue'
+import {useRoute} from 'vue-router'
+import { ref, watch, computed } from 'vue';
+import quizes from '../data/quizes.json'
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find(q => q.id === quizId);
+const currentQuestionIndex =ref(0);
+const numberOfCorrectAnswers= ref(0)
+const showResults = ref(false)
+
+// const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`);
+
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+// })
+
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`);
+const barPercentage = computed(() => `${currentQuestionIndex.value/quiz.questions.length * 100}%`)
+
+const onOptionSelected = (isCorrect) =>{
+    if(isCorrect){
+        numberOfCorrectAnswers.value++
+    }
+
+    if(quiz.questions.length - 1 === currentQuestionIndex.value){
+        showResults.value = true
+    }
+    
+    currentQuestionIndex.value++
+}
+</script>
+
 <template>
     <div>
-        <header>
-            <h4>Question </h4>
-            <div class="bar">
-                <div class="completion"></div>
-            </div>
-        </header>
+        <QuizHeader
+        :questionStatus="questionStatus"
+        :barPercentage="barPercentage"
+        />
         <div>
-            <div class="question-container">
-                <h1 class="question">
-                    what is the cheical value of table salt
-                </h1>
-            </div>
-            <div class="options-container">
-                <div class="option">
-                    <p class="option-label">A</p>
-                    <div class="option-value">
-                        <p>NaCl</p>
-                    </div>
-                </div>
-            </div>
-            <div class="options-container">
-                <div class="option">
-                    <p class="option-label">B</p>
-                    <div class="option-value">
-                        <p>NaCl</p>
-                    </div>
-                </div>
-            </div>
-            <div class="options-container">
-                <div class="option">
-                    <p class="option-label">C</p>
-                    <div class="option-value">
-                        <p>NaCl</p>
-                    </div>
-                </div>
-            </div>
-            <div class="options-container">
-                <div class="option">
-                    <p class="option-label">D</p>
-                    <div class="option-value">
-                        <p>NaCl</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
+            <Question 
+                v-if="!showResults"
+                :question="quiz.questions[currentQuestionIndex]"
+                @selectOption="onOptionSelected"
+            />
+            <Result 
+                v-else
+                :quizQuestionlength="quiz.questions.length"
+                :numberOfCorrectAnswers="numberOfCorrectAnswers"
+            />
+        </div>        
     </div>
 </template>
-
-<style scoped>
-header {
-     margin-top: 20px;
-}
-header h4{
-    font-size:30px; 
-
-}
-.bar{
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-}
-.completion{
-    height: 100%;
-    width: 0%;
-    background: bisque;
-}
-
-.question-container{
-    margin-top: 20px;
-}
-.question{
-    font-size: 40px;
-    margin-bottom: 20px;
-}
-.option{
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-}
-.option-label{
-    background-color: bisque;
-    width: 50px;
-    height: 50px;
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-.option-value{
-    background: rgb(244,239,239);
-    width: 100%;
-    font-size: 30px;
-    padding: 0 20px;
-}
-</style>
-<!-- {{ quiz.questions }} -->
